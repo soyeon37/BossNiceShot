44a953e7e96ec99725acb9d806ca3b989db4a978
김성규 <tjdrb9999@naver.com>
2023-07-17 17:46:31 +0900

    Style: Swagger Config 주석추가

--- BE/web/src/main/java/com/ssafy/config/swagger/SwaggerConfig.java
+++ BE/web/src/main/java/com/ssafy/config/swagger/SwaggerConfig.java
@@ -1,6 +1,6 @@
 package com.ssafy.config.swagger;
 
-// Java
+// Java.
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
