83e16a925e1e679b46cb6b715897496e5c44044c
최현우 <kaisa6361@naver.com>
2023-07-18 17:58:20 +0900

    Feat: texteditor 삽입 및 글장성 페이지 제작
    Former-commit-id: 92c03f11347956f27778b00424cc81dc055eefb3

--- FE/golffire-react/src/pages/community/Community.js
+++ FE/golffire-react/src/pages/community/Community.js
@@ -1,14 +1,21 @@
 import React from "react";
 import MyEditor from "./MyEditor";
 
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const Community = () => {
   return (
-    <div>
-      <h1 style={{ fontSize: "50px", padding: "20%" }}>
+    <div style={{ display:"flex", flexDirection:"column", alignItems:"center"}}>
+      <h1 style={{ fontSize: "50px", padding: "20" }}>
         Community
       </h1>
       <MyEditor />
+      {/* 버튼을 만들었는데 div tag가 겹칩니다. */}
+
+      <div style={{  display:"flex", justifyContent:"space-between", position:"static", padding:"20", width:"200px", alignContent:"center"}}>
+        <Button>취소</Button>
+        <Button>등록하기</Button>
+      </div>
     </div>
   );
 };
