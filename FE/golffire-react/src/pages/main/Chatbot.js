cc6c751414e44d3741288926ccdd19dfbd67a523
김재아 <rkddnjs2816@naver.com>
2023-07-18 18:02:57 +0900

    Feat : Chatbot js 생성

---
+++ FE/golffire-react/src/pages/main/Chatbot.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ChatBot from 'react-simple-chatbot';
+
+function Chatbot() {
+    return (
+        <div style={{ backgroundColor: "red" }}>
+
+        </div>
+    );
+}
+
+export default Chatbot;
