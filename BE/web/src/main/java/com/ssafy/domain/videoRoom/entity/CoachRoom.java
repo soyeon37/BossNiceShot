cfdd8050cd88a0c8f96e45a2e22c0925a83630be
김령은 <leongeun@naver.com>
2023-07-19 17:39:48 +0900

    Feat : Web RTC Config Setting
    Former-commit-id: 33ed2f3ea2fcdcf408250c22add96363e35b8b83

---
+++ BE/web/src/main/java/com/ssafy/domain/videoRoom/entity/CoachRoom.java
@@ -0,0 +1,17 @@
+//package com.ssafy.domain.coachingroom.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//import org.springframework.data.annotation.CreatedDate;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Getter
+//@Builder
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor
+//public class  {
+//
+//
+//}
