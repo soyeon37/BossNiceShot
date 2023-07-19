9b5dc72b6c243fff8afeb636d58782227aca13b5
김재아 <rkddnjs2816@naver.com>
2023-07-19 18:15:31 +0900

    Feat : 검색화면 구조
    Former-commit-id: 70297b6525cce820886639817f69ee1ff351e6bd

--- FE/golffire-react/src/pages/golffield/Golffield.js
+++ FE/golffire-react/src/pages/golffield/Golffield.js
@@ -1,20 +1,38 @@
 import React from "react";
 import Kakao from "./Kakao";
 
+import { Select, Input } from '@chakra-ui/react'
 import "./Golffield.css";
 
 function Golffield() {
   return (
     <div id="Golffield">
       <div id="search-box">
-
-        검색창 공간 : 이름 검색 & 지역 검색
+        <div>
+          <Select placeholder='Select option'>
+            <option value='option1'>지역을 선택하세요</option>
+            <option value='option2'>서울</option>
+            <option value='option3'>강릉</option>
+          </Select>
+        </div>
+        <div>
+          <Input placeholder='검색어를 입력하세요' />
+        </div>
       </div>
 
-      <Kakao />
-      <div>
-        여기에 리스트
 
+      <div id="map-box">
+        <div id="kakao-map">
+          <Kakao />
+
+        </div>
+        <div id="field-list">
+          list <br />
+          list <br />
+          list <br />
+          list <br />
+          list <br />
+        </div>
       </div>
     </div>
   );
