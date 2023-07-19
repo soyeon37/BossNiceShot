9ebc8c912375e738186bf3c3a417b2cbe4dfa62d
김재아 <rkddnjs2816@naver.com>
2023-07-19 16:52:25 +0900

    Feat : 골프장 carousel 아래에 바로가기 추가
    Former-commit-id: 1d1e7b3e14e6bc3d15c5578e18d7b862d4e88c7c

--- FE/golffire-react/src/pages/main/Main.js
+++ FE/golffire-react/src/pages/main/Main.js
@@ -3,6 +3,8 @@ import MainBanner from "./MainBanner";
 import SplideGolf from "./SplideGolf";
 import Chatbot from "./Chatbot";
 
+import "./Main.css";
+
 function Main() {
   return (
     <div id="Home" style={{ height: "580px" }}>
