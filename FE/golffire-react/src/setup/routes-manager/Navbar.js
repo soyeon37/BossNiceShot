4176a96ae2750e557c2c8bd15a0152dbaf475c63
김재아 <rkddnjs2816@naver.com>
2023-07-20 16:37:07 +0900

    Fix : 로그인 여부에 따라 드롭다운 변경
    Former-commit-id: 2c2b73d3d1399a4fa0f6b681938f53ee04d29f45

--- FE/golffire-react/src/setup/routes-manager/Navbar.js
+++ FE/golffire-react/src/setup/routes-manager/Navbar.js
@@ -1,4 +1,6 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useCookies } from 'react-cookie';
+
 import "./styles.css"
 import { Icon } from '@chakra-ui/icons'
 import {
@@ -17,8 +19,19 @@ import {
 import { IoMdContact } from 'react-icons/io'
 import { NavLink } from "react-router-dom"
 
-
 function Navbar() {
+    // cookie의 user 정보 확인
+    const [cookies] = useCookies(['user']);
+    // 로그인 여부를 나타내는 변수, false로 초기화
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // 로그인 여부를 확인하여 true로 바꾸는 함수
+    useEffect(() => {
+        // 로그인 정보가 있다면 true, 없다면 false로 만드는 코드
+        setIsLoggedIn(!!cookies.user);
+        // 추후 서버로 token 정보를 보내 유효한지 확인한 뒤 true로 만들기
+    }, [cookies]);
+
     return (
         <nav className="nav">
             <a href="/" className="site-title">
@@ -63,8 +76,8 @@ function Navbar() {
                 <li className="mypagemenu">
                     <Menu>
                         <MenuButton as={IconButton} icon={<IoMdContact fontSize="30px" />} />
-                        <MenuList>
-                            <MenuGroup title='프로필'>
+                        <MenuList>{isLoggedIn ? (
+                            <MenuGroup title=''>
                                 <MenuItem>
                                     <NavLink to="/signup/" style={({ isActive, isPending }) => {
                                         return {
@@ -76,19 +89,19 @@ function Navbar() {
                                 </MenuItem>
                                 <MenuItem>친구/채팅</MenuItem>
                             </MenuGroup>
-                            <MenuDivider />
-                            <MenuGroup title='Help'>
-                                <MenuItem>
-                                    <NavLink to="/login/" style={({ isActive, isPending }) => {
-                                        return {
-                                            fontWeight: isActive ? "bold" : "",
-                                        };
-                                    }}>
-                                        로그인
-                                    </NavLink>
-                                </MenuItem>
-                                <MenuItem>회원가입</MenuItem>
-                            </MenuGroup>
+
+                        ) : (<MenuGroup title=''>
+                            <MenuItem>
+                                <NavLink to="/login/" style={({ isActive, isPending }) => {
+                                    return {
+                                        fontWeight: isActive ? "bold" : "",
+                                    };
+                                }}>
+                                    로그인
+                                </NavLink>
+                            </MenuItem>
+                            <MenuItem>회원가입</MenuItem>
+                        </MenuGroup>)}
                         </MenuList>
                     </Menu>
                 </li>
