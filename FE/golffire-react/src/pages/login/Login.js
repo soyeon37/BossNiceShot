25f0ee70d1651b02e37d8178f7bf308c681ccf3f
김재아 <rkddnjs2816@naver.com>
2023-07-20 09:51:55 +0900

    Feat : Login 페이지 생성
    Former-commit-id: 2e4ead96da484b80a2677f8956616c068b5ee02d

---
+++ FE/golffire-react/src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Login() {
+  return (
+    <div id="Login" style={{height:"800px"}}>
+      <h1 style={{fontSize:"50px",padding:"20%"}}>
+      Login
+      </h1>
+    </div>
+  );
+};
+
+export default Login;
