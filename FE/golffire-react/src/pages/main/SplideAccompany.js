2b1fe8b23de0745b852f301c9b4a96ace368c714
김재아 <rkddnjs2816@naver.com>
2023-07-21 16:47:25 +0900

    Fix : carousel 추가 시도
    Former-commit-id: 66d4d0df9be87fc1b1048198585aa9604e75ce66

---
+++ FE/golffire-react/src/pages/main/SplideAccompany.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PartGolf from "./slide-part/PartGolf";
+
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+import { NavLink } from "react-router-dom"
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
+function Carousel({ props }) {
+  return (
+    <div id="splide-golf">
+      
+    </div>
+  );
+}
+
+export default Carousel;
