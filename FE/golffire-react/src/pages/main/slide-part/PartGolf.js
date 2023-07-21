8e2d965994ef7298eb0ec2b740cf41d792b75fd1
김재아 <rkddnjs2816@naver.com>
2023-07-21 17:42:57 +0900

    Fix : Carousel에 데이터 자동 대입
    Former-commit-id: fdbf10a29c3af9b1dc9a23b8e634f79b1c678ba7

--- FE/golffire-react/src/pages/main/slide-part/PartGolf.js
+++ FE/golffire-react/src/pages/main/slide-part/PartGolf.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-function PartGolf() {
-    return (
-      <div className="golf-info">
-        <div className="golf-name">
-            하이골프클럽(골프장 이름)
-        </div>
-        <div className="golf-add">
-            서울특별시 서초구 잠원동 신반포로47길 77 두원빌딩 1층(골프장 주소)
-        </div>
+function PartGolf({ name, address }) {
+  return (
+    <div className="golf-info">
+      <div className="golf-name">
+        {name}
       </div>
-    );
-  }
-  
-  export default PartGolf;
+      <div className="golf-add">
+        {address}</div>
+    </div>
+  );
+}
+
+export default PartGolf;
