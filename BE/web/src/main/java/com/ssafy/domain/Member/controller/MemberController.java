c41d6df90da59cbb90fd2435725ba98d25317f51
함소연 <soyeun3377@naver.com>
2023-07-25 11:10:29 +0900

    Feat: Kakao Login API 회원가입 구현 및 사용자 기본 기능 수정
    Former-commit-id: 0364be4372bdac3cd80b07a3e5ad6cba8a224404

--- BE/web/src/main/java/com/ssafy/domain/Member/controller/MemberController.java
+++ BE/web/src/main/java/com/ssafy/domain/Member/controller/MemberController.java
@@ -2,12 +2,8 @@ package com.ssafy.domain.Member.controller;
 
 import com.ssafy.Exception.message.ExceptionMessage;
 import com.ssafy.Exception.model.UserAuthException;
-import com.ssafy.domain.Member.dto.request.SendEmailRequest;
-import com.ssafy.domain.Member.dto.request.SignInRequest;
-import com.ssafy.domain.Member.dto.request.SignUpRequest;
-import com.ssafy.domain.Member.dto.request.UpdateMemberRequest;
+import com.ssafy.domain.Member.dto.request.*;
 import com.ssafy.domain.Member.dto.response.SignInResponse;
-import com.ssafy.domain.Member.repository.MemberRepository;
 import com.ssafy.domain.Member.service.MemberService;
 import com.ssafy.config.ApiResponse;
 import com.ssafy.domain.Member.service.RefreshTokenService;
@@ -16,13 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 
 
 @Slf4j
@@ -31,7 +27,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/members")
 public class MemberController {
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
     private final RefreshTokenService refreshTokenService;
 
@@ -45,7 +40,7 @@ public class MemberController {
 
     @PostMapping("/sendEmailVerification")
     public ApiResponse sendEmailVerification(@RequestBody SendEmailRequest request ){
-
+        log.info("이메일 전송 시작");
         return ApiResponse.success(memberService.sendEmail(request));
     }
 
@@ -69,25 +64,18 @@ public class MemberController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @PostMapping("/sign-in")
-    public ApiResponse signIn(@RequestBody SignInRequest request, HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
-
+    public ApiResponse signIn(@RequestBody SignInRequest request) {
+        log.info("로그인 시작");
         SignInResponse signInResponse = memberService.signIn(request);
-//        ResponseCookie cookie = ResponseCookie.from("RefreshToken", signInResponse.token().getRefreshToken())
-//                .maxAge(7 * 24 * 60 * 60) // 유효 기간 7일 후 만료
-//                .path("/")
-//                .secure(true) // https 환경에서만 쿠키 발동
-//                .sameSite("None") // 동일 및 크로스 사이트 쿠키 전송 가능
-//                .httpOnly(true)
-//                .build();
-//        servletResponse.setHeader("Set-Cookie", cookie.toString());
         // Redis에 저장
-        refreshTokenService.setValues(signInResponse.token().getRefreshToken(), request.memberId());
-        log.info("memberId={}",request.memberId());
-        log.info("refreshToken={}",signInResponse.token().getRefreshToken());
-
+        refreshTokenService.setValues(signInResponse.token().getRefreshToken(), request.id());
+        log.info("memberId={}, refreshToken={}",request.id(), signInResponse.token().getRefreshToken());
         return ApiResponse.success(signInResponse);
     }
 
+    /**
+     * 로그아웃 시, FrontEnd에서 AccessToken 삭제
+     */
     @Operation(summary = "로그아웃", description = "로그아웃")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
@@ -96,18 +84,20 @@ public class MemberController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @PostMapping("/logout")
-        public ApiResponse logout (HttpServletRequest servletRequest, String accessToken){
-                    Cookie[] list = servletRequest.getCookies();
-                    if(list != null){
-                        for(Cookie cookie : list){
-                            refreshTokenService.delValues(cookie.getValue());
-                            log.info("cookie={}",cookie);
-                log.info("cookie={}",cookie.getValue());
-                cookie.setMaxAge(0);
+        public ApiResponse logout (HttpServletRequest servletRequest){
+            Cookie[] list = servletRequest.getCookies();
+            if(list != null){
+                for(Cookie cookie : list){
+                    if(cookie.getName().equals("refreshToken")){
+                        log.info("cookieValue={}", cookie.getValue());
+                        refreshTokenService.delValues(cookie.getValue()); // Redis에 저장된 refreshToken 삭제
+                        cookie.setMaxAge(0); // Cookie에 저장된 refreshToken 삭제
+                    }
+                }
             }
-        }
-        return ApiResponse.success("");
+        return ApiResponse.success("SUCCESS");
     }
+
 
     @Operation(summary = "회원 정보 수정", description = "회원 정보를 수정한다.")
     @ApiResponses({
@@ -117,28 +107,30 @@ public class MemberController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @PutMapping("/update")
-    public ApiResponse updateMember (@RequestBody UpdateMemberRequest request, HttpServletRequest servletRequest){
-        // temp
-        Cookie[] list = servletRequest.getCookies();
-        String memberId = "";
-        if(list != null){
-            for (Cookie cookie:list) {
-                if (cookie.getName().equals("RefreshToken")) {
-                    // 있으면 redis에 저장된 member 정보 반환
-                    log.info("Cookie={}", cookie.getValue());
-                    memberId = refreshTokenService.getValues(cookie.getValue());
-                    log.info("memberId={}", memberId);
-                }
-            }
-        }
+    public ApiResponse updateMember (@RequestBody UpdateMemberRequest request, Principal principal){
+        String memberId =  principal.getName();
         return ApiResponse.success(memberService.updateMember(request, memberId));
     }
 
+    @Operation(summary = "회원 정보 삭제", description = "회원 정보를 삭제한다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자 없음"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @DeleteMapping("/delete")
     public ApiResponse deleteMember (String memberId){
         return ApiResponse.success(memberService.deleteMember(memberId));
     }
 
+    @Operation(summary = "회원 정보 추출", description = "회원 정보를 DB에서 가져온다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자 없음"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @GetMapping("/info")
     public ApiResponse info(Authentication authentication){
         if (authentication == null || authentication.getName() == null){
@@ -146,23 +138,30 @@ public class MemberController {
         }
         return ApiResponse.success(memberService.findByMemberId(authentication.getName()));
     }
-    @PostMapping("/authorize")
-    public ApiResponse authorize(@RequestHeader("Authorization") String accessToken, Authentication authentication){
-        if (authentication == null || authentication.getName() == null) {
-            throw new UserAuthException(ExceptionMessage.NOT_AUTHORIZED_ACCESS);
-        }
-        return ApiResponse.success(memberService.getAuthorize(accessToken));
-    }
 
+    @Operation(summary = "Token 재발급", description = "만료된 Token을 재발급 한다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "인증 실패"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자 없음"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/reissue")
-    public ApiResponse reissue(@RequestBody String refreshToken){
-        log.info("ReIssue");
-        refreshToken = refreshToken.substring(0, refreshToken.length()-1);
-        log.info("ReIssue={}", refreshToken);
+    public ApiResponse reissue(@RequestBody ReIssueRequest request){
+        log.info("토큰 재발급 시작");
+        String token = request.refreshToken();
+        String refreshToken = token.substring(0, token.length()-1);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return ApiResponse.success(memberService.reissue(refreshToken, authentication));
     }
 
+//    @PostMapping("/authorize")
+//    public ApiResponse authorize(@RequestHeader("Authorization") String accessToken, Authentication authentication){
+//        if (authentication == null || authentication.getName() == null) {
+//            throw new UserAuthException(ExceptionMessage.NOT_AUTHORIZED_ACCESS);
+//        }
+//        return ApiResponse.success(memberService.getAuthorize(accessToken));
+//    }
 
 }
 
--- BE/web/src/main/java/com/ssafy/domain/Member/service/MemberService.java
+++ BE/web/src/main/java/com/ssafy/domain/Member/service/MemberService.java
@@ -4,7 +4,6 @@ import com.ssafy.Exception.message.ExceptionMessage;
 import com.ssafy.Exception.model.TokenCheckFailException;
 import com.ssafy.Exception.model.UserAuthException;
 import com.ssafy.config.security.jwt.JwtTokenProvider;
-import com.ssafy.config.security.jwt.RefreshToken;
 import com.ssafy.domain.Member.dto.request.SendEmailRequest;
 import com.ssafy.domain.Member.dto.request.UpdateMemberRequest;
 import com.ssafy.domain.Member.dto.response.*;
@@ -13,20 +12,9 @@ import com.ssafy.domain.Member.repository.MemberRepository;
 import com.ssafy.config.security.jwt.TokenInfo;
 import com.ssafy.domain.Member.dto.request.SignInRequest;
 import com.ssafy.domain.Member.dto.request.SignUpRequest;
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.mail.MailAuthenticationException;
-import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -35,7 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.Properties;
 import java.util.Random;
 
 //import jakarta.mail
@@ -58,7 +45,7 @@ public class MemberService{
     @Transactional
     public SignUpResponse registMember(SignUpRequest request) {
         log.info(request.toString());
-        if(findByMemberId(request.memberId())!= null){
+        if(findByMemberId(request.id())!= null){
             throw new IllegalArgumentException("이미 사용중인 아이디입니다.");
         }
         Member member = memberRepository.save(Member.from(request, encoder));
@@ -74,7 +61,7 @@ public class MemberService{
 
         // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
         // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.memberId(), request.password());
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.id(), request.password());
         log.info("authenticationToken={}", authenticationToken);
 
         // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
@@ -83,7 +70,7 @@ public class MemberService{
         log.info("authentication={}", authentication);
 
         // 2-1. 비밀번호 체크
-        Optional<Member> member = memberRepository.findById(request.memberId());
+        Optional<Member> member = memberRepository.findById(request.id());
         if(!encoder.matches(request.password(), member.get().getPassword())) {
             return null;
         }
@@ -116,45 +103,54 @@ public class MemberService{
     }
 
     @Transactional
-    public AuthorizeResponse getAuthorize(String accessToken){
+    public ReIssueResponse getAuthorize(String accessToken){
 
-        return AuthorizeResponse.from(accessToken, "GET_AUTHORIZE");
+        return ReIssueResponse.from(accessToken, "GET_AUTHORIZE");
     }
 
     @Transactional
-    public AuthorizeResponse reissue(String refreshToken, Authentication authentication) {
+    public ReIssueResponse reissue(String refreshToken, Authentication authentication) {
+        // 1. 인가 이름 확인
         log.info("authentication.getName={}", authentication.getName());
         if (authentication == null || authentication.getName() == null) {
             throw new UserAuthException(ExceptionMessage.NOT_AUTHORIZED_ACCESS);
         }
         log.info("refreshToken={}", refreshToken);
-        // refreshToken 만료 검사
+        // 2. refreshToken 만료 검사
         if(!jwtTokenProvider.validateToken(refreshToken)){
-            // RefreshToken 만료 시, Redis에서 삭제
+            // 2-1. refreshToken 만료 시, Redis에서 삭제
+            // Front에서 Logout 실행
             refreshTokenService.delValues(refreshToken);
-            return AuthorizeResponse.from(null, "INVALID_REFRESH_TOKEN");
+            return ReIssueResponse.from(null, "INVALID_REFRESH_TOKEN");
         }
+        // 3. Redis에서 refreshToken 가져오기
         String id = refreshTokenService.getValues(refreshToken);
-        log.info("refreshToken Id={}", id);
+        log.info("id from redis={}", id);
+        // 4. 권한 검사
         if(id == null || !id.equals(authentication.getName())){
             throw new TokenCheckFailException(ExceptionMessage.MISMATCH_TOKEN);
         }
-        return createRefreshToken(refreshToken, authentication);
+        return createAccessToken(refreshToken, authentication);
     }
 
-    private AuthorizeResponse createRefreshToken(String refreshToken, Authentication authentication){
+    /**
+     * RefreshToken으로 AccessToken 재발급
+     */
+    private ReIssueResponse createAccessToken(String refreshToken, Authentication authentication){
+        // 5. RefreshToken의 만료 기간 확인
         if (jwtTokenProvider.checkExpiredToken(refreshToken)){
             TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
-            log.info(tokenInfo.getAccessToken());
-            return AuthorizeResponse.from(tokenInfo.getAccessToken(), "SUCCESS");
+            // 6. RefreshToken으로 AccessToke 발급 성공
+            return ReIssueResponse.from(tokenInfo.getAccessToken(), "SUCCESS");
         }
-        return AuthorizeResponse.from(jwtTokenProvider.generateToken(authentication).getAccessToken(),"GENERAL_FAILURE");
+        // RefreshToken 발급 실패
+        return ReIssueResponse.from(jwtTokenProvider.generateToken(authentication).getAccessToken(),"GENERAL_FAILURE");
     }
 
     public SendEmailResponse sendEmail(SendEmailRequest request) {
         // 111111 ~ 999999 6자리 랜덤 난수 생성
         int authNumber = makeAuthNum();
-        log.info(request.Id());
+        log.info(request.id());
         String content =
                 "홈페이지를 방문해주셔서 감사합니다." + 	//html 형식으로 작성 !
                         "<br><br>" +
@@ -162,7 +158,7 @@ public class MemberService{
                         "<br>" +
                         "해당 인증번호를 인증번호 확인란에 기입하여 주세요."; //이메일 내용 삽입
         try{
-            emailService.sendMail(FROM_EMAIL, (String) request.Id(), "이메일 인증 메일", content);
+            emailService.sendMail(FROM_EMAIL, (String) request.id(), "이메일 인증 메일", content);
 
         }catch (Exception e){
 
