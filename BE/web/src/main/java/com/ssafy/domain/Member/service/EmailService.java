a387ff8df166dfeec5638b29055a1e861eceb5f6
함소연 <soyeun3377@naver.com>
2023-07-25 11:10:29 +0900

    Feat: Kakao Login API 회원가입 구현 및 사용자 기본 기능 수정
    Former-commit-id: 766565135a9a1006c0dba43426641b75a524635a

--- BE/web/src/main/java/com/ssafy/domain/Member/service/EmailService.java
+++ BE/web/src/main/java/com/ssafy/domain/Member/service/EmailService.java
@@ -6,7 +6,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
