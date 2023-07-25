285e655145be72448b8d6f7421a1753b2f8209c8
김재아 <rkddnjs2816@naver.com>
2023-07-25 23:45:15 +0900

    Feat : 회원가입 페이지 틀 완성
    Former-commit-id: 7b109d720a80abf11738ee0db5c6877aaffab8db

--- FE/golffire-react/src/pages/signup/SignupInfo.js
+++ FE/golffire-react/src/pages/signup/SignupInfo.js
@@ -1,16 +1,77 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { React, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-function SignupInfo() {
+import { Button, FormControl, FormLabel, Input, Radio, RadioGroup, VStack } from "@chakra-ui/react";
+
+const SignupInfo = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (value) => {
+    setSelectedOption(value);
+  };
+
+  const navigate = useNavigate();
+  const handleEmailFinish = () => {
+    navigate("/");
+  };
+
   return (
     <div id="Signup">
-      <div className="title">
-        <h1>
-          회원가입 - 정보 채우기
-        </h1>
+      <div className="signup-info">
+        <div className="signup-title">
+          <h1>
+            정보 입력
+            <br />
+            자신을 소개하는 정보를 입력하세요.
+          </h1>
         </div>
+        <div className="signup-pic">(사진 공간)</div>
+        <div className="signup-info-body">
+          <FormControl maxW={"sm"}>
+            <FormLabel>닉네임</FormLabel>
+            <Input placeholder="닉네임을 입력하세요." bg={"white"} />
+            {/* 사용 가능 여부를 나타내는 안내 문구 필요! */}
+          </FormControl>
+
+          <FormControl maxW={"sm"}>
+            <FormLabel>자기소개</FormLabel>
+            <Input placeholder="자기소개를 입력하세요." bg={"white"} />
+            {/* 입력 제한 안내 문구 필요! */}
+          </FormControl>
+
+          <FormControl maxW={"sm"}>
+            <FormLabel>최고타수</FormLabel>
+            <Input placeholder="0" bg={"white"} />
+            <FormLabel>평균타수</FormLabel>
+            <Input placeholder="0" bg={"white"} />
+          </FormControl>
+
+          <RadioGroup value={selectedOption} onChange={handleOptionChange}>
+            <VStack spacing={4}>
+              <Radio value="option1">레드티박스 - 여성</Radio>
+              <Radio value="option2">화이트티박스 - 남성</Radio>
+              <Radio value="option3">블랙티박스 - 프로</Radio>
+            </VStack>
+          </RadioGroup>
+        </div>
+        <Button
+          onClick={handleEmailFinish}
+          style={{
+            height: "2.5rem",
+            width: "100%",
+
+            color: "black",
+            borderRadius: "30px",
+            background: "#B8F500",
+          }}
+          maxW={"sm"}
+          marginBottom={"2.5rem"}
+        >
+          회원가입 완료하기
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default SignupInfo;
