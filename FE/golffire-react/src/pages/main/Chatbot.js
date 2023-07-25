bdf24591d7274f71ebbf34065d2a777db75c890a
김재아 <rkddnjs2816@naver.com>
2023-07-25 23:11:12 +0900

    Feat : Chatbot Widget
    Former-commit-id: 088f83081565576cb8133307fcd70837dc0b2f80

--- FE/golffire-react/src/pages/main/Chatbot.js
+++ FE/golffire-react/src/pages/main/Chatbot.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { ChatIcon } from "@chakra-ui/icons";
+
 // 챗봇
 import Notifications from "react-push-notification";
 import NotificationComponent from "./chatbot/NotificationComponent";
@@ -7,15 +9,23 @@ import SimpleChatBox from "./chatbot/SimpleChatBox";
 import SimpleForm from "./chatbot/form/SimpleForm";
 
 function Chatbot() {
-    return (
-        <div className="Chatbot">
-            <div className="chat-widget"></div>
-            {/* <Notifications /> */}
-            {/* <NotificationComponent /> */}
-            {/* <SimpleChatBox /> */}
-            {/* <SimpleForm /> */}
-        </div>
-    );
+  return (
+    <div className="Chatbot">
+      {/* <div className="chat-widget"></div> */}
+
+      {/* 챗봇 위젯 동그라미 버튼 */}
+      <div class="chatbot-widget">
+        <span class="chatbot-icon">
+          <ChatIcon />
+        </span>
+      </div>
+
+      {/* <Notifications /> */}
+      {/* <NotificationComponent /> */}
+      {/* <SimpleChatBox /> */}
+      {/* <SimpleForm /> */}
+    </div>
+  );
 }
 
 export default Chatbot;
