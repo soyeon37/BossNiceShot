d05bf5f17f8c2e4e948107f146b619fd37cfa2b4
김령은 <leongeun@naver.com>
2023-07-28 17:58:40 +0900

    Feat : Companion entity
    Former-commit-id: 78c773b551b65102fbb29cf43466020ce40d015b

--- BE/web/src/main/java/com/ssafy/domain/Companion/entity/Companion.java
+++ BE/web/src/main/java/com/ssafy/domain/Companion/entity/Companion.java
@@ -1,4 +1,19 @@
 package com.ssafy.domain.Companion.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Companion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companionId;
+
+    @Column(columnDefinition = )
+
 }
