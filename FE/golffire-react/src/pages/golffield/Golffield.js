19d4edc4e2af71cd640a7143f07e670c8c331b40
김재아 <rkddnjs2816@naver.com>
2023-07-28 17:57:33 +0900

    Feat : 검색 기능 구현
    Former-commit-id: 6186d92219a3e460feb2008f072c956b4f003cee

--- FE/golffire-react/src/pages/golffield/Golffield.js
+++ FE/golffire-react/src/pages/golffield/Golffield.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import KakaoMap from "./KakaoMap";
-
-import { Select, Input } from "@chakra-ui/react";
 import GolfBox from "./GolfBox";
+
+import { SearchIcon } from '@chakra-ui/icons';
 import "./Golffield.css";
 
 function Golffield() {
+  // 출력할 골프장 정보 변수
   const golfClubs = [
     {
       name: "하이골프클럽11",
@@ -25,36 +26,90 @@ function Golffield() {
     // Add more golf club data objects here
   ];
 
+  // 지역 검색 필터 변수
+  const areas = [
+    { id: '0', area: "전국", },
+    { id: '1', area: "서울", },
+    { id: '2', area: "경기", },
+    { id: '3', area: "강원", },
+  ];
+
+  const [area, setArea] = useState(areas[0].id);
+  const [searchWord, setSearchWord] = useState('');
+
+  const handleSelectArea = (e) => {
+    setArea(e.target.value);
+  }
+
+  // 검색창 내 문자 변화 감지
+  const handleSearchWord = (e) => {
+    setSearchWord(e.target.value);
+  }
+  
+  // 엔터 키 눌림 감지
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      doSearchWord();
+    }
+  };
+
+  // 검색
+  const doSearchWord = () => {
+    console.log(searchWord);
+  }
+
   return (
     <div id="Golffield">
-      <div id="search-box">
-        <div>
-          <Select placeholder="Select option">
-            <option value="option1">지역을 선택하세요</option>
-            <option value="option2">서울</option>
-            <option value="option3">강릉</option>
-          </Select>
-        </div>
-        <div>
-          <Input placeholder="검색어를 입력하세요" />
-        </div>
+      <div id="golf-title">
+        골프장 검색
       </div>
-
-      <div id="result-box">
-        <div id="kakao-map">
-          <KakaoMap />
+      <div id="golf-search">
+        <div id="search-box">
+          <select
+            value={area}
+            id="areaSelector"
+            onChange={handleSelectArea}
+          >
+            {areas.map((item) => (
+              <option key={item.id} value={item.area}>
+                {item.area}
+              </option>
+            ))}
+          </select>
+          <input
+            id="searchWord"
+            defaultValue={searchWord}
+            onChange={handleSearchWord}
+            onKeyDown={handleKeyDown}
+            placeholder="검색어를 입력하세요"
+          />
+          <button
+            className="icon-div"
+            onClick={doSearchWord}
+          >
+            <SearchIcon boxSize={6} />
+          </button>
+        </div>
+        <div id="result-box">
+          
         </div>
 
-        <div id="result-list">
-          {golfClubs.map((club, index) => (
-            <GolfBox
-              key={index}
-              name={club.name}
-              address={club.address}
-              phoneNumber={club.phoneNumber}
-            />
-          ))}
-        </div>
+        {/* <div id="result-box">
+          <div id="kakao-map">
+            <KakaoMap />
+          </div>
+
+          <div id="result-list">
+            {golfClubs.map((club, index) => (
+              <GolfBox
+                key={index}
+                name={club.name}
+                address={club.address}
+                phoneNumber={club.phoneNumber}
+              />
+            ))}
+          </div>
+        </div> */}
       </div>
     </div>
   );
