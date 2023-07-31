fd005d753d0c0a52e890e83bb25b325d132f4d8b
김재아 <rkddnjs2816@naver.com>
2023-07-31 10:32:09 +0900

    Feat : Merge all
    Former-commit-id: 7207547567ccd6497ae7590ed97824d4938aa616

--- FE/golffire-react/src/pages/login/Login.js
+++ FE/golffire-react/src/pages/login/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
         axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
 
         // 쿠키에 정보 저장
-        setCookie('Set-Cookie', refresh_token, { path: '/' , maxAge: new Date().getDate() + 60 * 60 * 24 *14 });
+        setCookie('Set-Cookie', refresh_token, { path: '/', maxAge: new Date().getDate() + 60 * 60 * 24 * 14 });
 
         console.log(response.data); // Debug Code !!
 
@@ -60,11 +60,6 @@ const Login = () => {
     const REST_API_KEY = 'cd0c9cf0cf49dae9a987aebb769ee0d6';
     const REDIRECT_URI = 'http://localhost:3000/auth/kakao/login/callback'
     const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-    window.location.href=kakaoUrl;
-
-    const REST_API_KEY = "cd0c9cf0cf49dae9a987aebb769ee0d6";
-    const REDIRECT_URI = "http://localhost:3000/auth/kakao/login/callback";
-    const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     window.location.href = kakaoUrl;
   };
 
