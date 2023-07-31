42a79212d900000dab66721cf392f2de96a89887
youngeun_kim <leongeun@naver.com>
2023-08-01 01:22:23 +0900

    Merge :commit before change directory
    Former-commit-id: 17809124d044a1d25546abe9a7e27d77bee17277

--- BE/web/src/main/java/com/ssafy/domain/Companion/repository/CompanionRepository.java
+++ BE/web/src/main/java/com/ssafy/domain/Companion/repository/CompanionRepository.java
@@ -1,6 +1,9 @@
 package com.ssafy.domain.Companion.repository;
 
+import com.ssafy.common.TeeBox;
 import com.ssafy.domain.Companion.entity.Companion;
+import com.ssafy.domain.chat.entity.ChatParticipant;
+import com.ssafy.domain.follow.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,12 +12,17 @@ public interface CompanionRepository extends JpaRepository<Companion, Long> {
 
     //제목으로 검색
     List<Companion> findByTitle(String title);
+
     //작성자로 검색
+    List<Companion> findByMemberId(String memberId);
+
     //제목 + 내용으로 검색
+    List<Companion> findByTitleAndContents(String title, String contents);
 
     //티박스로 검색
+    List<Companion> findByTeeBox(TeeBox teeBox);
 
     //팔로잉한 사람들이 동행 모집을 올렸는지 검색
-
+//    List<Companion> findCompanionByFollowerId(String followerId);
 
 }
--- BE/web/src/main/java/com/ssafy/domain/Companion/repository/CompanionUserRepository.java
+++ BE/web/src/main/java/com/ssafy/domain/Companion/repository/CompanionUserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanionUserRepository extends JpaRepository<CompanionUser, Long> {
     //컴패니언 기반으로 불러오는 것
+
     // 유저 기반으로 불러오는 것
 }
