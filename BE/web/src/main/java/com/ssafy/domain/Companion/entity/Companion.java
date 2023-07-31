32c5d71e1eb561cfd77f4412a5c1aba2214d568b
김령은 <leongeun@naver.com>
2023-07-31 16:03:09 +0900

    Feat : Company Entity
    Former-commit-id: 5042f67e6d63275debe44672869bb9d6b650abf5

--- BE/web/src/main/java/com/ssafy/domain/Companion/entity/Companion.java
+++ BE/web/src/main/java/com/ssafy/domain/Companion/entity/Companion.java
@@ -1,9 +1,15 @@
 package com.ssafy.domain.Companion.entity;
 
+import com.ssafy.domain.Member.entity.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -12,8 +18,43 @@ public class Companion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long companionId;
 
-    @Column(columnDefinition = )
+    @NotNull
+    @Column(name = "title", length = 50)
+    private String title;
+
+    @Column(name = "introduce_Content", nullable = true, columnDefinition = "TEXT", length = 300)
+    private String contents;
+
+    @Column(nullable = false)
+    private int field;
+
+    @Column(name = "tee_Box" , nullable = false)
+    private int teeBox;
+
+    @Column(name = "aim_People", nullable = false)
+    private int aimPeople;
+
+    @Column(name = "current_People", nullable = false)
+    private int currentPeople;
+
+    //티업날짜
+    @Column(name = "teeup_Date", nullable = false)
+    private Date teeupDate;
+
+    //마감날짜
+    @Column(name = "end_Date", nullable = false)
+    private Date endDate;
+
+    //동행 모집 작성한 날짜
+    @CreatedDate
+    @Column(name = "created_Date")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
 }
