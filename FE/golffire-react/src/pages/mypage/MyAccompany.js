67ebe65dfd80dbf14f2443e76d8f52e999d82e4d
김재아 <rkddnjs2816@naver.com>
2023-07-31 16:10:42 +0900

    Feat : 마이페이지 틀 생성
    Former-commit-id: 11540cbf39e76b7ff92cfca5f696f2673b06a84b

--- FE/golffire-react/src/pages/mypage/MyAccompany.js
+++ FE/golffire-react/src/pages/mypage/MyAccompany.js
@@ -5,12 +5,18 @@ import "./MyPage.css";
 function MyAccompany() {
     return (
         <div id="MyPage">
-
             <MyPageNavbar />
             <div id="MyAccompany">
-            MyAccompany
+                <div id="myacc-title">
+                    나의 동행
+                </div>
+                <div id="myacc-toggle">
+                    모집/신청
+                </div>
+                <div id="myacc-list">
+                    여기에 리스트 형식으로 보임
+                </div>
             </div>
-
         </div >
     );
 }
--- FE/golffire-react/src/pages/mypage/Profile.js
+++ FE/golffire-react/src/pages/mypage/Profile.js
@@ -5,12 +5,37 @@ import "./MyPage.css";
 function Profile() {
     return (
         <div id="MyPage">
-
             <MyPageNavbar />
             <div id="Profile">
-                Profile
+                <div id="profile-info">
+                    <div id="info-pic">(커다란 캐릭터 사진)</div>
+                    <div id="info-text">
+                        <div id="info-email">이메일</div>
+                        <div id="info-nickname">닉네임</div>
+                        <div id="info-introduction">자기소개</div>
+                        <div id="info-golf-info1">
+                            <div id="info-golf-info1-1">
+                                최고타수
+                            </div>
+                            <div id="info-golf-info1-2">
+                                평균타수
+                            </div>
+                        </div>
+                        <div id="info-golf-info2">
+                            <div id="info-golf-info2-1">
+                                레벨
+                            </div>
+                            <div id="info-golf-info2-2">
+                                선호 티박스
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="profile-history">
+                    <div id="history-title">나의 스코어</div>
+                    <div id="history-list">스코어 리스트(component 사용 예정)</div>
+                </div>
             </div>
-
         </div >
     );
 }
