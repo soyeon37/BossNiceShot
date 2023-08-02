ae5a9a754cc1079f735a2c980835cef887e9bf05
김재아 <rkddnjs2816@naver.com>
2023-08-02 16:21:09 +0900

    Feat : 채팅 내역 출력
    Former-commit-id: 1e160170ac65a1935f30a364df3dc884150a5ecf

--- FE/golffire-react/src/pages/mypage/MyChat.js
+++ FE/golffire-react/src/pages/mypage/MyChat.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 import MyPageNavbar from "./MyPageNavbar";
+
+import ListRoom from "./components/ListRoom";
 import ChatRoom from "./components/ChatRoom";
+
+import TeeRed from "../../assets/source/icons/flag-red.png";
+import TeeWhite from "../../assets/source/icons/flag-white.png";
+import TeeBlack from "../../assets/source/icons/flag-black.png";
+import TeeAll from "../../assets/source/icons/flag-all.png";
+
 import "./MyPage.css";
 
 function MyChat() {
     const [searchWord, setSearchWord] = useState("");
+    const [roomId, setRoomId] = useState("0");
+
+    const teeMap = {
+        red: TeeRed,
+        white: TeeWhite,
+        black: TeeBlack,
+        all: TeeAll,
+    }
 
     // 채팅방(참여하고 있는 동행 그룹) 리스트 정보
     const chatRoomsData = [
@@ -32,7 +48,7 @@ function MyChat() {
         {
             id: 4,
             title: "Chat Room 4",
-            tee: "unknown",
+            tee: "all",
             field: "Field 4",
             date: "2023-08-05",
         },
@@ -43,6 +59,14 @@ function MyChat() {
         setSearchWord(e.target.value);
         console.log("검색어: ", searchWord);
     };
+
+    // 리스트에서 선택된 방의 채팅방 보이기
+    const handleRoomClick = (roomId) => {
+        setRoomId(roomId);
+        console.log("data: ", chatRoomsData);
+    };
+
+    const selectedoomData = chatRoomsData.find((chatRoomsData) => chatRoomsData.id === roomId);
 
     return (
         <div id="MyPage">
@@ -61,21 +85,29 @@ function MyChat() {
                         </div>
                         <div id="chat-list">
                             {chatRoomsData.map((chatRoomData) => (
-                                <ChatRoom
-                                    key={chatRoomData.id}
-                                    title={chatRoomData.title}
-                                    tee={chatRoomData.tee}
-                                    field={chatRoomData.field}
-                                    date={chatRoomData.date}
-                                />
+                                <div key={chatRoomData.id} onClick={() => handleRoomClick(chatRoomData.id)}>
+                                    <ListRoom
+                                        title={chatRoomData.title}
+                                        tee={teeMap[chatRoomData.tee]}
+                                        field={chatRoomData.field}
+                                        date={chatRoomData.date}
+                                    />
+                                </div>
                             ))}
-                            </div>
+                        </div>
                     </div>
                     <div id="chat-room">
-                        <div id="room-title"></div>
-                        <div id="room-text"></div>
-                        <div id="room-input"></div>
+                        {roomId === "0" ? (
+                            <>
+                                <div id="room-title">채팅창을 선택하세요.</div>
+                            </>
+                        ) : (
+                            <>
+                                <ChatRoom props={selectedoomData} />
+                            </>
+                        )}
                     </div>
+
                 </div>
             </div>
         </div >
