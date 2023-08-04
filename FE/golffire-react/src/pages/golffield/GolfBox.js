e072c405e420dc2f25a78b93810e8feb4c4fabb9
김재아 <rkddnjs2816@naver.com>
2023-08-04 18:08:42 +0900

    Feat : 검색 결과
    Former-commit-id: 54944eaecdbc87bbbabc37bdf7d3389572c3a7f2

--- FE/golffire-react/src/pages/golffield/GolfBox.js
+++ FE/golffire-react/src/pages/golffield/GolfBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function GolfBox({ name, address, callNumber }) {
+import NaverLogo from "../../assets/source/icons/naver_icon.png"
+function GolfBox({ id, name, address, callNumber, setCenter }) {
   // 네이버 검색창 연결 함수
   const searchOnNaver = () => {
     const naverSearchUrl = `https://search.naver.com/search.naver?query=` + name;
@@ -8,9 +9,13 @@ function GolfBox({ name, address, callNumber }) {
   }
 
   return (
-    <div className="GolfBox"
-      onClick={() => searchOnNaver()}>
-      <div className="golfbox-name">{name}</div>
+    <div className="GolfBox" onClick={() => setCenter(id)}>
+      <div className="golfbox-title">
+        <div className="golfbox-name">{name}</div>
+        <div className="golfbox-naver" onClick={() => searchOnNaver()}>
+          <img src={NaverLogo} alt={`네이버 ` + name + ` 검색 결과`} />
+          </div>
+      </div>
       <div className="golfbox-add">{address ? address : '등록된 주소가 없습니다.'}</div>
       <div className="golfbox-call">{callNumber}</div>
     </div>
--- FE/golffire-react/src/pages/golffield/KakaoMap.js
+++ FE/golffire-react/src/pages/golffield/KakaoMap.js
@@ -2,24 +2,34 @@ import React, { useEffect } from "react";
 
 const { kakao } = window;
 
-function KakaoMap() {
+function KakaoMap({ centerId, getLocations, getCenterId }) {
+  const defaultLatLng = [37.50127843363402, 127.03960748710476]
+  console.log("카카오가 받은 거?vgetLocations: ", getLocations);
+
+  const checktmp = () => {
+    // console.log("지도를 클릭함", centerId);
+  }
+
   useEffect(() => {
     const container = document.getElementById("map");
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
+      // 37.50127843363402, 127.03960748710476
+
+      center: new kakao.maps.LatLng(defaultLatLng[0], defaultLatLng[1]),
+      level: 5,
     };
     const map = new kakao.maps.Map(container, options);
 
     // 중심 마커 표시
-    const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+    console.log("kakaomap: ",getCenterId);
+    let markerPosition = new kakao.maps.LatLng(37.50127843363402, 127.03960748710476);
     const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
     marker.setMap(map);
   }, []);
 
-  return <div id="map"></div>;
+  return <div id="map" onClick={checktmp}></div>;
 }
 
 export default KakaoMap;
