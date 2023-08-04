6a6ef5f87b47685ae4aa0f9d194834f4c8170df5
김재아 <rkddnjs2816@naver.com>
2023-08-04 16:23:30 +0900

    Feat : 클릭 시 네이버 검색 연결?
    Former-commit-id: f891cb5ba4ceed230adf629992fd7c21a8a42c3f

--- FE/golffire-react/src/pages/golffield/GolfBox.js
+++ FE/golffire-react/src/pages/golffield/GolfBox.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function GolfBox({ name, address, callNumber }) {
+  // 네이버 검색창 연결 함수
+  const searchOnNaver = () => {
+    const naverSearchUrl = `https://search.naver.com/search.naver?query=` + name;
+    window.open(naverSearchUrl, '_blank');
+  }
+
   return (
-    <div className="GolfBox">
+    <div className="GolfBox"
+      onClick={() => searchOnNaver()}>
       <div className="golfbox-name">{name}</div>
       <div className="golfbox-add">{address ? address : '등록된 주소가 없습니다.'}</div>
       <div className="golfbox-call">{callNumber}</div>
