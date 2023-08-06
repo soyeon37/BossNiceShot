a7a97e56a02efb50927dd48f2a38085335a146cd
김재아 <rkddnjs2816@naver.com>
2023-08-06 23:13:31 +0900

    Feat : 주소를 통해 위도 및 경도 값 변환
    Former-commit-id: 20de7502701607adf445ba6f8eea3f8d4d0e5cfc

--- FE/golffire-react/src/pages/golffield/KakaoMap.js
+++ FE/golffire-react/src/pages/golffield/KakaoMap.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import proj4 from "proj4";
 
 const { kakao } = window;
 
 function KakaoMap({ centerId, getId, getAddress, getLatLng }) {
+  // const [centerLatLng, setCenterLatLng] = useState([33.450701, 126.570667]);
+
   // Proj4의 좌표계 변환을 위한 변수
   const fromProjection = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs"; // Web Mercator (수학 좌표계)
   // const fromProjection = "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43"
@@ -15,30 +17,61 @@ function KakaoMap({ centerId, getId, getAddress, getLatLng }) {
   // console.log("allAddress: ", allAddress);
   // console.log("allLatLng: ", allLatLng);
 
-  // allLatLng 값 중 없는 값이 있다면 allAddress 찾기
-  // let geocoder = new kakao.maps.services.Geocoder();
-  // console.log(geocoder);
-
   // 현재 중심이 되는 위치의 id 찾기
   let curIndex = allId.indexOf(centerId);
   curIndex = curIndex === -1 ? 0 : curIndex;
   // console.log("curIndex: ", curIndex);
+
+  // allLatLng 값 중 없는 값이 있다면 allAddress 찾기
+  const parseLatLng = (address) => {
+    const geocoder = new kakao.maps.services.Geocoder();
+    geocoder.addressSearch(address, function (result, status) {
+      // 정상적으로 검색이 완료됐으면 
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        console.log("변환 테스트 결과, coords: ", coords);
+        var resultLatLng = [coords.Ma, coords.La]
+        console.log("resultLatLng: ", resultLatLng)
+        return resultLatLng;
+        // return ["", ""]
+      } else {
+        console.log("변환 에러남")
+        return ["", ""]
+      }
+    });
+
+    return ["", ""]
+  }
 
   // 현재 중심 좌표를 반환하는 함수
   const getCurCenter = () => {
     let centerLatLng = [33.450701, 126.570667] // 기본 중심 좌표(카카오 본사)
     const x = allLatLng[curIndex * 2];
     const y = allLatLng[curIndex * 2 + 1];
-    console.log("원점 좌표계? ", x, ", ", y)
+    // console.log("원점 좌표계? ", x, ", ", y)
     if (x) {
       const LngLat = proj4(fromProjection, toProjection, [x, y]);
-      console.log("변환된 값? ", LngLat[1], LngLat[0]);
+      // console.log("변환된 값? ", LngLat[1], LngLat[0]);
       // centerLatLng = [37.18567591064152, 127.40153163660632];
       centerLatLng = [LngLat[1], LngLat[0]]
       // console.log("내가 따로 넣은 값? ", centerLatLng);
+    } else {
+      const curAdd1 = allAddress[curIndex * 2];
+      const curAdd2 = allAddress[curIndex * 2 + 1];
+      let curAdd = curAdd1 ? curAdd1 : curAdd2;
+      if (curAdd) {
+        console.log("파싱할 주소: ", curAdd);
+        const parsedLatLng = parseLatLng(curAdd);
+        console.log("위경도가 없어서 주소 파싱한 결과: ", parsedLatLng);
+        centerLatLng = parsedLatLng;
+        return [37.1324673675995, 127.522027682876]
+      } else {
+        console.log("주소까지 없는 곳,,, 뭐냐 여긴");
+      }
     }
+
     // centerLatLng = allLatLng[curIndex * 2] ? [allLatLng[curIndex * 2], allLatLng[curIndex * 2 + 1]] : centerLatLng;
-    console.log("global centerLatLng: ", centerLatLng);
+    // console.log("global centerLatLng: ", centerLatLng);
     return centerLatLng;
   }
 
@@ -53,7 +86,6 @@ function KakaoMap({ centerId, getId, getAddress, getLatLng }) {
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
-
 
     // for (var i = 0; i < allLatLng.length; i += 2) {
     //   var marker = new kakao.maps.Marker({
