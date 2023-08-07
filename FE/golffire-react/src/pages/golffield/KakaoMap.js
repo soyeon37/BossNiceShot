ff2293b0e7fe7220c7defb325eaef5a81d3d114a
김재아 <rkddnjs2816@naver.com>
2023-08-07 10:27:08 +0900

    Fix : 골프장 완성
    Former-commit-id: ea6d92f679ad24a04373b7c67b4ea62f8ab5999b

--- FE/golffire-react/src/pages/golffield/KakaoMap.js
+++ FE/golffire-react/src/pages/golffield/KakaoMap.js
@@ -4,101 +4,87 @@ import proj4 from "proj4";
 const { kakao } = window;
 
 function KakaoMap({ centerId, getId, getAddress, getLatLng }) {
-  // const [centerLatLng, setCenterLatLng] = useState([33.450701, 126.570667]);
+  // 이전 골프장 정보를 저장하기 위한 변수
+  const [ centerLatLng, setCenterLatLng ] = useState(["", ""]);
 
   // Proj4의 좌표계 변환을 위한 변수
   const fromProjection = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs"; // Web Mercator (수학 좌표계)
-  // const fromProjection = "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43"
   const toProjection = 'EPSG:4326'; // WGS 84 (위도, 경도)
 
   const allId = getId;
   const allAddress = getAddress;
   const allLatLng = getLatLng;
-  // console.log("allAddress: ", allAddress);
-  // console.log("allLatLng: ", allLatLng);
 
   // 현재 중심이 되는 위치의 id 찾기
   let curIndex = allId.indexOf(centerId);
   curIndex = curIndex === -1 ? 0 : curIndex;
   // console.log("curIndex: ", curIndex);
-
+  // console.log("주소1: ", allAddress[curIndex * 2]);
+  // console.log("주소2: ", allAddress[curIndex * 2 + 1]);
+  // console.log("좌표 정보: ", allLatLng[curIndex * 2], ", ", allLatLng[curIndex * 2 + 1]);
+  
   // allLatLng 값 중 없는 값이 있다면 allAddress 찾기
   const parseLatLng = (address) => {
     const geocoder = new kakao.maps.services.Geocoder();
-    geocoder.addressSearch(address, function (result, status) {
-      // 정상적으로 검색이 완료됐으면 
-      if (status === kakao.maps.services.Status.OK) {
+    geocoder.addressSearch(address, function (result, status) { 
+      if (status === kakao.maps.services.Status.OK) { // 정상적으로 검색 완료
         var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-        console.log("변환 테스트 결과, coords: ", coords);
         var resultLatLng = [coords.Ma, coords.La]
-        console.log("resultLatLng: ", resultLatLng)
         return resultLatLng;
-        // return ["", ""]
-      } else {
-        console.log("변환 에러남")
-        return ["", ""]
+      } else { // 변환 값 에러
+        return [null, null]
       }
     });
-
-    return ["", ""]
+    return [null, null]
   }
 
   // 현재 중심 좌표를 반환하는 함수
   const getCurCenter = () => {
-    let centerLatLng = [33.450701, 126.570667] // 기본 중심 좌표(카카오 본사)
+    let parsedLatLng = ["", ""]
     const x = allLatLng[curIndex * 2];
     const y = allLatLng[curIndex * 2 + 1];
-    // console.log("원점 좌표계? ", x, ", ", y)
-    if (x) {
+    
+    if (x) { // [x, y] => [위도, 경도]
       const LngLat = proj4(fromProjection, toProjection, [x, y]);
-      // console.log("변환된 값? ", LngLat[1], LngLat[0]);
-      // centerLatLng = [37.18567591064152, 127.40153163660632];
-      centerLatLng = [LngLat[1], LngLat[0]]
-      // console.log("내가 따로 넣은 값? ", centerLatLng);
-    } else {
+      parsedLatLng = [LngLat[1], LngLat[0]]
+    } else { // 주소 => [위도, 경도]
       const curAdd1 = allAddress[curIndex * 2];
       const curAdd2 = allAddress[curIndex * 2 + 1];
-      let curAdd = curAdd1 ? curAdd1 : curAdd2;
-      if (curAdd) {
-        console.log("파싱할 주소: ", curAdd);
-        const parsedLatLng = parseLatLng(curAdd);
-        console.log("위경도가 없어서 주소 파싱한 결과: ", parsedLatLng);
-        centerLatLng = parsedLatLng;
-        return centerLatLng;
-        // return [37.1324673675995, 127.522027682876]
-      } else {
-        console.log("주소까지 없는 곳,,, 뭐냐 여긴");
-        return [37.1324673675995, 127.522027682876]
+      
+      if(!parsedLatLng[0] && curAdd1) {
+        parsedLatLng = parseLatLng(curAdd1);
+        // console.log("첫 주소(", curAdd1, ")로 변환된 좌표: ", parsedLatLng);
+      }
+      if(parsedLatLng[0] == null && curAdd2){
+        parsedLatLng = parseLatLng(curAdd2);
+        // console.log("안돼서 두번째 주소(", curAdd2, ")로 변환된 좌표: ", parsedLatLng);
+      }
+      if (parsedLatLng[0] == null) {
+        // console.log("어떻게 해도 변환이 되지 않음 => ", parsedLatLng);
       }
     }
-
-    // centerLatLng = allLatLng[curIndex * 2] ? [allLatLng[curIndex * 2], allLatLng[curIndex * 2 + 1]] : centerLatLng;
-    // console.log("global centerLatLng: ", centerLatLng);
-    return centerLatLng;
+    return parsedLatLng;
   }
 
   useEffect(() => {
     const container = document.getElementById("map");
-    const centerPosition = getCurCenter();
-    console.log("centerPosition: ", getCurCenter());
+    let centerPosition = getCurCenter();
+    
+    // 좌표 정보가 없을 경우, 기존 골프장 정보로 설정
+    if (centerPosition[1]) {
+      setCenterLatLng(centerPosition);
+    } else {
+      centerPosition = centerLatLng;
+    }
 
+    // 지도의 중심을 골프장 위치로 변경
     const options = {
-      // center: new kakao.maps.LatLng(33.450701, 126.570667),
       center: new kakao.maps.LatLng(centerPosition[0], centerPosition[1]),
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
 
-    // for (var i = 0; i < allLatLng.length; i += 2) {
-    //   var marker = new kakao.maps.Marker({
-    //     // map: map,
-    //     position: new kakao.maps.LatLng(allLatLng[i], allLatLng[i + 1]),
-    //   });
-    //   // console.log(" allLatLng :", allLatLng)
-    //   console.log("찍힌 마커의 좌표: ", marker.position)
-    //   marker.setMap(map);
-    // }
-
+    // 골프장 위치에 marker 표시
     let markerPosition = new kakao.maps.LatLng(centerPosition[0], centerPosition[1]);
     const marker1 = new kakao.maps.Marker({
       map: map,
@@ -106,9 +92,6 @@ function KakaoMap({ centerId, getId, getAddress, getLatLng }) {
     });
     marker1.setMap(map);
 
-    //
-    // console.log("지도 다시 그리기");
-    // map.setCenter(new kakao.maps.LatLng(getCurCenter[0], getCurCenter[1]));
   }, [centerId, getId, getAddress, getLatLng]);
 
   return (
