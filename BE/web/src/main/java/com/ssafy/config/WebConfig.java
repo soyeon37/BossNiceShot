692f1c092fd554b04876f24ce43824a6e3828f86
한라연 <gksfkdus@naver.com>
2023-08-07 13:54:48 +0900

    Feat: CORS 설정
    Former-commit-id: 3aa73e6548799a33ce5f528753aab0b59d0b013c

--- BE/web/src/main/java/com/ssafy/config/WebConfig.java
+++ BE/web/src/main/java/com/ssafy/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://172.30.32.1:3000");
     }
 }
