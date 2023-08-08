e113b0f6865876fde4c66da80351f7dff3114fc0
김재아 <rkddnjs2816@naver.com>
2023-08-08 17:51:05 +0900

    Feat : main carousel 추가
    Former-commit-id: 968fddd78da432cea64aae6ffb2813a8fdca1c77

--- FE/golffire-react/src/pages/main/slide-part/PartAccompany.js
+++ FE/golffire-react/src/pages/main/slide-part/PartAccompany.js
@@ -1,19 +1,11 @@
 import React from "react";
 
-function PartAccompany({ user_name, title, address, member_num, deadline }) {
+function PartAccompany({ title, golf_num, date }) {
   return (
     <div className="acc-info">
-      <div className="acc-title">{title} </div>
-      <div className="acc-detail">
-        <div className="acc-detail-box">
-          <div className="acc-detail-left">{address}</div>
-          <div className="acc-detail-right">{member_num}</div>
-        </div>
-        <div className="acc-detail-box">
-          <div className="acc-detail-left">{user_name}</div>
-          <div className="acc-detail-right">{deadline}</div>
-        </div>
-      </div>
+      <div className="acc-title">{title}</div>
+      <div className="acc-address">{golf_num}</div>
+      <div className="acc-date">{date}</div>
     </div>
   );
 }
