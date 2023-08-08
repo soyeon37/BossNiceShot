88d5cdc2d2323e6e8acc0cc6e9e1dd8bfe7b5008
김재아 <rkddnjs2816@naver.com>
2023-08-08 18:25:53 +0900

    Feat : carousel용 골프장 이름 가져오기(실패)
    Former-commit-id: afb4690c0e19eb6b09a4008245320db6431b0afb

--- FE/golffire-react/src/pages/main/slide-part/PartAccompany.js
+++ FE/golffire-react/src/pages/main/slide-part/PartAccompany.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function PartAccompany({ title, golf_num, date }) {
+function PartAccompany({ title, golf_place, date }) {
+  console.log("내가 받은 정보? ", title, golf_place, date);
+  
   return (
     <div className="acc-info">
       <div className="acc-title">{title}</div>
-      <div className="acc-address">{golf_num}</div>
+      <div className="acc-place">{golf_place}</div>
       <div className="acc-date">{date}</div>
     </div>
   );
