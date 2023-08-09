afaffeb824f108ab2956641ba5bc02bb903618b4
한라연 <gksfkdus@naver.com>
2023-08-09 14:42:16 +0900

    Feat: CORS 오리진 패턴 설정
    Former-commit-id: 50e4a430aad1af0ecb160dd1ac1f68fafa677664

--- BE/web/src/main/java/com/ssafy/config/WebConfig.java
+++ BE/web/src/main/java/com/ssafy/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:3000", "http://172.30.32.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
