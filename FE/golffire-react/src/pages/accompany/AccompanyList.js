224b5f57b1238b958752a10c80c4705fad80603d
김재아 <rkddnjs2816@naver.com>
2023-08-12 15:37:36 +0900

    Fix : CSS 변경사항 반영
    Former-commit-id: 6e83152d48fe4f9af79a16047fe2c3e53ef138bd

--- FE/golffire-react/src/pages/accompany/AccompanyList.js
+++ FE/golffire-react/src/pages/accompany/AccompanyList.js
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import AccompanySearch from "./AccompanySearch";
-
-import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
-import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
+import { SearchIcon, ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 
 function AccompanyList() {
+
+  const [searchValue, setSearchValue] = useState("");
+  const [searchFilter, setSearchFilter] = useState("title");
+  const [selectedFollow, setSelectedFollow] = useState(false);
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+    // 검색 기능을 추가하고 원하는 작업을 수행할 수 있습니다.
+  };
+
+  const handleFilterChange = (event) => {
+    setSearchFilter(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    // 검색 버튼을 클릭할 때 서버로 검색 필터와 검색 값 전송하는 로직을 추가합니다.
+    if (searchValue.trim() !== "") {
+      const searchData = {
+        filter: searchFilter,
+        value: searchValue,
+      };
+
+      // 여기서 searchData를 서버로 전송하는 API 호출 등의 작업을 수행할 수 있습니다.
+      console.log("Sending searchData to server:", searchData);
+    }
+  };
+
+  // 팔로잉 라디오 버튼 함수
+  const handleFollowChange = () => {
+    setSelectedFollow(!selectedFollow);
+  }
+
+  // 이 위는 검색 필터 기능들
+  // 이 아래는 리스트 관련 기능들
   const [accompanyList, setAccompanyList] = useState([{}, {}, {}, {}, {}, {}]);
 
   // 동행 모집 리스트
@@ -79,17 +110,54 @@ function AccompanyList() {
   };
 
   return (
-    <div className="accompanylist-container">
-      <div className="accompanylist">
-        <div className="accompanylist-head">
+    <div className="list-container">
+      <div className={isJoining ? 'list-container-list-selected' : 'list-container-list-unselected'}>
+        <div className="list-head">
           <Link to="/createaccompany">
-            <div className="head-create-button">새 동행 모집하기</div>
+            <div className="head-create-button bg-accompany">+ 모집하기</div>
           </Link>
-          <AccompanySearch />
-          <Checkbox colorScheme="green">팔로잉</Checkbox>
+
+          <div className="search-container">
+            {/* 검색창 */}
+            <input
+              className="search-input-box"
+              type="text"
+              value={searchValue}
+              onChange={handleInputChange}
+              placeholder="검색어를 입력하세요"
+            />
+            <button id="search-icon" onClick={handleSearchClick}>
+              <SearchIcon boxSize={6} />
+            </button>
+
+            {/* 검색 필터 */}
+            <select
+              id="searchFilter"
+              className="search-filter"
+              value={searchFilter}
+              onChange={handleFilterChange}
+            >
+              <option value="title">제목</option>
+              <option value="author">작성자</option>
+              <option value="titleAndContent">제목+내용</option>
+            </select>
+
+          </div>
+          <div className="checkbox-div">
+            <label>
+              <input
+                className="checkbox"
+                type="checkbox"
+                checked={selectedFollow}
+                onChange={handleFollowChange}
+              />
+              팔로잉
+            </label>
+          </div>
+
         </div>
 
-        <div className="accompanylist-body">
+        <div className="list-body">
           {getCurrentPageItems().map((accompanyRoom) => (
             <div className="accompany-room" key={accompanyRoom.id}>
               <div className="accroom-title">
@@ -105,7 +173,7 @@ function AccompanyList() {
           ))}
         </div>
 
-        <div className="accompanylist-footer">
+        <div className="list-footer">
           <button
             className="control-arrow"
             disabled={isFirstPage}
@@ -123,31 +191,60 @@ function AccompanyList() {
           </button>
         </div>
       </div>
+
+      {/* 선택 시 나타나는 정보 */}
       {isJoining && selectedRectangle && (
-        <div className="joining-room">
-          <div className="joining-title">{selectedRectangle.title}</div>
-          <div className="joining-author">
-            <div className="joining-author-box">작성자: {selectedRectangle.author}</div>
+        <div className="selected-container">
+          <div className="selected-container-head">
+            <div className="selected-container-title">
+              <div className="title-text">
+                글 제목
+              </div>
+              <div
+                className="title-close"
+                onClick={() => setIsJoining(false)}
+              >
+                X
+              </div>
+            </div>
+            <div className="selected-container-user">
+              글 작성자
+            </div>
           </div>
-          <div className="joining-description">
-            방설명{selectedRectangle.description} {/* 설명이 여기에 들어감 */}
+          <div className="selected-container-body">
+            <div>내용</div>
+            <div>동행 일시</div>
+            <div>골프장</div>
+            <div>참여 인원</div>
+            <div>티 박스</div>
           </div>
-          <div className="joining-button">
-            <Link to={`/accompanyroom/${selectedRectangle.id}`}>
-              <button>참여하기</button>
-            </Link>
+
+          <div className="selected-container-footer">
+            <button className=""> 참여하기</button>
           </div>
+
         </div>
+
+        // <div className="joining-room">
+        //   <div onClick={() => setIsJoining(false)}>닫기</div>
+        //   <div className="joining-title">{selectedRectangle.title}</div>
+        //   <div className="joining-author">
+        //     <div className="joining-author-box">작성자: {selectedRectangle.author}</div>
+        //   </div>
+        //   <div className="joining-description">
+        //     방설명{selectedRectangle.description} {/* 설명이 여기에 들어감 */}
+        //   </div>
+        //   <div className="joining-button">
+        //     <Link to={`/accompanyroom/${selectedRectangle.id}`}>
+        //       <button>참여하기</button>
+        //     </Link>
+        //   </div>
+
+        // </div>
       )}
 
       {/* 배경 div */}
-      {isJoining ? (
-        // 방 선택 시
-        <div className="selected-box"></div>
-      ) : (
-        // 방 미선택 시
-        <div className="unselected-box"></div>
-      )}
+      <div className="list-background-div bg-accompany"></div>
     </div>
   );
 }
