c5bfc72510de1d2f1924f3d62a026a754e7f35bc
김재아 <rkddnjs2816@naver.com>
2023-08-14 01:07:31 +0900

    Feat : 글 작성 부 생성
    Former-commit-id: b62560c03d1b49da1c68d79edb98864669c1a733

--- FE/golffire-react/src/pages/accompany/Accompany.js
+++ FE/golffire-react/src/pages/accompany/Accompany.js
@@ -7,7 +7,7 @@ import "./Accompany.css";
 function Accompany() {
   return (
     <div id="accompany-container" className="container">
-      <div className="container-head shadow-accompany">
+      <div className="container-head">
         <div className="container-head-title">동행</div>
         <div className="container-head-desc">
           원하는 날짜와 장소에 같이 갈 동행을 구해보아요.
