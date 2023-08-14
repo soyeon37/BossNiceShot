c5a8e49fb8382fd5afd7c29fcd29c8fee44f7b86
yohwan <moonyh9600@gmail.com>
2023-08-14 09:23:45 +0900

    Feat: 스트리밍, 채팅기능 추가
    Former-commit-id: 03e4955b506287b7fea40311ffedca770b4672fd

--- FE/golffire-react/src/openvidu/toolbar/ToolbarComponent.js
+++ FE/golffire-react/src/openvidu/toolbar/ToolbarComponent.js
@@ -4,18 +4,16 @@ import './ToolbarComponent.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
-import Mic from '@material-ui/icons/Mic';
-import MicOff from '@material-ui/icons/MicOff';
-import Videocam from '@material-ui/icons/Videocam';
-import VideocamOff from '@material-ui/icons/VideocamOff';
-// import Fullscreen from '@material-ui/icons/Fullscreen';
-// import FullscreenExit from '@material-ui/icons/FullscreenExit';
-import ScreenShare from '@material-ui/icons/ScreenShare';
-import StopScreenShare from '@material-ui/icons/StopScreenShare';
-import Tooltip from '@material-ui/core/Tooltip';
-import PowerSettingsNew from '@material-ui/icons/PowerSettingsNew';
-import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
-
+import { 
+	MdVideocam,
+	MdVideocamOff,
+	MdMic,
+	MdMicOff,
+	MdScreenShare,
+	MdStopScreenShare, 
+} from "react-icons/md";
+// import Tooltip from '@material-ui/core/Tooltip';
+// import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
 import IconButton from '@material-ui/core/IconButton';
 
 export default class ToolbarComponent extends Component {
@@ -26,8 +24,6 @@ export default class ToolbarComponent extends Component {
 		this.micStatusChanged = this.micStatusChanged.bind(this);
 		this.screenShare = this.screenShare.bind(this);
 		this.stopScreenShare = this.stopScreenShare.bind(this);
-		// this.toggleFullscreen = this.toggleFullscreen.bind(this);
-		// this.leaveSession = this.leaveSession.bind(this);
 		this.toggleChat = this.toggleChat.bind(this);
 	}
 
@@ -48,16 +44,6 @@ export default class ToolbarComponent extends Component {
 		this.props.stopScreenShare();
 	}
 
-	// toggleFullscreen() {
-	// 	this.setState({ fullscreen: !this.state.fullscreen });
-	// 	this.props.toggleFullscreen();
-	// }
-
-	// leaveSession() {
-	// 	this.props.leaveSession();
-	// 	window.location.href = '/studylist';
-	// }
-
 	toggleChat() {
 		this.props.toggleChat();
 	}
@@ -66,7 +52,8 @@ export default class ToolbarComponent extends Component {
 		const mySessionId = this.props.sessionId;
 		const localUser = this.props.user;
 		return (
-			<AppBar className="toolbar" id="header">
+			
+			// <AppBar className="toolbar" id="header">
 				<Toolbar className="toolbar">
 					{/* 방 제목 */ }
 					{/* <div id="navSessionInfo">
@@ -84,9 +71,9 @@ export default class ToolbarComponent extends Component {
 							onClick={this.micStatusChanged}
 						>
 							{localUser !== undefined && localUser.isAudioActive() ? (
-								<Mic /> 
+								<MdMic /> 
 							) : (
-								<MicOff color="secondary" />
+								<MdMicOff  />
 							)}
 						</IconButton>
 
@@ -97,9 +84,9 @@ export default class ToolbarComponent extends Component {
 							onClick={this.camStatusChanged}
 						>
 							{localUser !== undefined && localUser.isVideoActive() ? (
-								<Videocam />
+								<MdVideocam />
 							) : (
-								<VideocamOff color="secondary" />
+								<MdVideocamOff />
 							)}
 						</IconButton>
 
@@ -109,34 +96,18 @@ export default class ToolbarComponent extends Component {
 								className="navButton"
 								onClick={this.screenShare}
 							>
-								<ScreenShare fontSize="large" />
+								<MdScreenShare fontSize="large" />
 							</IconButton>
 						)}
 						{localUser !== undefined && localUser.isScreenShareActive() && (
 							<IconButton onClick={this.stopScreenShare} id="navScreenButton">
-								<StopScreenShare 
+								<MdStopScreenShare 
 									color="secondary" 
 									fontsize="large" 
 								/>
 							</IconButton>
 						)}
-
 						{/* <IconButton 
-							color="inherit" 
-							className="navButton" 
-							onClick={this.toggleFullscreen}
-						>
-							{localUser !== undefined && this.state.fullscreen ? <FullscreenExit /> : <Fullscreen />}
-						</IconButton> */}
-						{/* <IconButton 
-							color="secondary" 
-							className="navButton" 
-							onClick={this.leaveSession} 
-							id="navLeaveButton"
-						>
-							<PowerSettingsNew />
-						</IconButton> */}
-						<IconButton 
 							color="inherit" 
 							onClick={this.toggleChat} 
 							id="navChatButton"
@@ -145,10 +116,10 @@ export default class ToolbarComponent extends Component {
 							<Tooltip title="Chat">
 								<QuestionAnswer />
 							</Tooltip>
-						</IconButton>
+						</IconButton> */}
 					</div>
-				</Toolbar>
-			</AppBar>
+			  </Toolbar>
+		//   </AppBar>
 		);
 	}
 }
