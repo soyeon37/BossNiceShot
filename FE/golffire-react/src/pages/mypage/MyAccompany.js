e48d5b0d6418deaebb57a83d1110fdf5e1cd7020
김재아 <rkddnjs2816@naver.com>
2023-08-15 16:42:07 +0900

    Fix : merging
    Former-commit-id: c5f7ff15124cb0110a70fc33386ce0b65bc13b94

--- FE/golffire-react/src/pages/mypage/MyAccompany.js
+++ FE/golffire-react/src/pages/mypage/MyAccompany.js
@@ -1,23 +1,110 @@
 import React from "react";
 import MyPageNavbar from "./MyPageNavbar";
 import "./MyPage.css";
-
+import RedTeeImg from "../../assets/source/icons/flag-red.png";
 function MyAccompany() {
+    const personInfo = [
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        },
+        {
+            title: "골프러버들",
+            day: "2023-09-10 11:20",
+            filed: "포천힐스",
+            teebox: "RED",
+            aimpeople: 4,
+            currentpeople: 2,
+        }
+    ]
     return (
         <div id="MyPage">
-            <MyPageNavbar />
-            <div id="MyAccompany">
-                <div id="myacc-title">
-                    나의 동행
-                </div>
-                <div id="myacc-toggle">
-                    모집/신청
-                </div>
-                <div id="myacc-list">
-                    여기에 리스트 형식으로 보임
+            <div id="MyPageBox">
+                <MyPageNavbar />
+                <div id="MyAccompany">
+                    <div id="myacc-title">
+                        나의 동행
+                    </div>
+                    <div id="myacc-toggle">
+                        모집/신청
+                    </div>
+                    <div id="myacc-list">
+                        <div id="myacc-body">
+                            {personInfo.map((companion, index) => (
+                                <div id="myacc-list-div" key={index}>
+                                    <div id="myacc-list-top">
+                                        <div id="myacc-list-top-div">
+                                            <div id="myacc-list-title">
+                                                {companion.title}
+                                            </div>
+                                            <div id="myacc-list-teebox">
+                                                <img id="teebox-img" src={RedTeeImg}></img>
+                                            </div>
+                                        </div>
+                                        <div id="myacc-list-field">
+                                            {companion.filed}
+                                        </div>
+                                        <div id="myacc-list-day">
+                                            {companion.day}
+                                        </div>
+                                    </div>
+                                    <div id="myacc-list-bottom">
+                                        <button id="myacc-list-button">
+                                            {companion.currentpeople}/{companion.aimpeople}
+                                        </button>
+
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 
--- FE/golffire-react/src/pages/mypage/MyPageNavbar.js
+++ FE/golffire-react/src/pages/mypage/MyPageNavbar.js
@@ -1,8 +1,15 @@
 import { color } from "framer-motion";
-
-import React from "react";
+import React from "react"
+import "./MyPage.css";
+import profileIMGn from "../../assets/source/imgs/favicon.png";
 
 import { NavLink, Navigate } from "react-router-dom"
+import { background } from "@chakra-ui/react";
+import { InfoOutlineIcon } from '@chakra-ui/icons'
+import { red } from "@mui/material/colors";
+import { IoGolfOutline } from "react-icons/io5";
+import { BsChatDots, BsFillTrashFill } from "react-icons/bs";
+import { MdPeopleOutline } from "react-icons/md";
 
 function MyPageNavbar() {
     return (
@@ -14,32 +21,50 @@ function MyPageNavbar() {
                 <div className="nav-user">
 
                     <div className="user-photo">
-                        <div style={{ width: "100px", height: "100px", borderRadius: "50px", backgroundColor: "red" }}></div>
+                        <div style={{ width: "150px", height: "150px", borderRadius: "50%", backgroundColor: "#FFE000", margin: "0 auto", border: "2px solid black" }}>
+                        <img src={profileIMGn}></img>
+                        </div>
                     </div>
-                    <div className="user-name">
+                    <div className="user-title">
                         <div className="user-level">B 배찌</div>
                         <div className="user-name">김싸피</div>
                     </div>
                 </div>
-                <li>
-                    <NavLink to="/mypage">내 정보</NavLink>
-                </li>
+                <div className="nav-list">
 
-                <li>
-                    <NavLink to="/mypage/myaccompany">동행</NavLink>
-                </li>
+                    <li id="nav-list-click">
+                        <NavLink to="/mypage/info"
+                            className={({ isActive }) => (isActive ? "active-link" : "nav-list-text")}
+                        >
+                            <div className="nav-icon"><InfoOutlineIcon/></div>
+                            <div>내 정보</div>
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/mypage/mychat">채팅</NavLink>
-                </li>
+                    <li id="nav-list-click">
+                        <NavLink to="/mypage/myaccompany"
+                            className={({ isActive }) => (isActive ? "active-link" : "nav-list-text")}
+                        >
+                            <div  className="nav-icon"><IoGolfOutline/></div>
+                            <div>동행</div>
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/mypage/myfollow">팔로우</NavLink>
-                </li>
+                    <li id="nav-list-click">
+                        <NavLink to="/mypage/mychat" className={({ isActive }) => (isActive ? "active-link" : "nav-list-text")}><div  className="nav-icon"><BsChatDots/></div>
+                            <div>채팅</div></NavLink>
+                    </li>
 
-                <li>
-                    <NavLink to="/mypage/signout">탈퇴하기</NavLink>
-                </li>
+                    <li id="nav-list-click">
+                        <NavLink to="/mypage/myfollow" className={({ isActive }) => (isActive ? "active-link" : "nav-list-text")}><div  className="nav-icon"><MdPeopleOutline/></div>
+                            <div>팔로우</div></NavLink>
+                    </li>
+
+                    <li id="nav-list-click">
+                        <NavLink to="/mypage/signout" className={({ isActive }) => (isActive ? "active-link-signout" : "nav-list-text-signout")}><div  className="nav-icon"><BsFillTrashFill/></div>
+                            <div>탈퇴하기</div></NavLink>
+                    </li>
+                </div>
 
             </nav>
         </div>
