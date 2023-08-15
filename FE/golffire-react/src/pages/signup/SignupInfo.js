d965eb7c309e584d9ac594f4dbef6ea935f730d2
김재아 <rkddnjs2816@naver.com>
2023-08-15 17:41:54 +0900

    Feat : 툴팁 완성
    Former-commit-id: 693fa058957bd62895e3d87e6d0623aa9b51d7e1

--- FE/golffire-react/src/pages/signup/SignupInfo.js
+++ FE/golffire-react/src/pages/signup/SignupInfo.js
@@ -11,19 +11,24 @@ import flagwhite from '../../assets/source/icons/flag-white.png';
 import flagblack from '../../assets/source/icons/flag-black.png';
 import flagall from '../../assets/source/icons/flag-all.png';
 
+import { Button, FormControl, FormLabel, Input, Radio, RadioGroup, VStack } from "@chakra-ui/react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const SignupInfo = () => {
   // Redux
+  const dispatch = useDispatch();
   const state = useSelector((state) => state.signupFeature);
 
-  const [email, setEmail] = useState(state.email);
+  // const { state } = useLocation();
+  const [image, setImage] = useState("../../assets/source/icons/no-image.png");
   const [password] = useState(state.password);
-  const [nickname, setNickname] = useState(state.nickname);
   const [introduce, setIntroduce] = useState("");
+  const [email, setEmail] = useState(state.email);
+  const [nickname, setNickname] = useState(state.nickname);
   const [averageScore, setAverageScore] = useState(state.averageScore);
   const [topScore, setTopScore] = useState(state.topScore);
   const [teeBox, setTeeBox] = useState(state.teeBox);
+  const [isKakao, setIsKakao] = useState(state.isKakao);
 
   // 닉네임 중복 검사
   const handleCheckNickname = () => {
@@ -31,7 +36,7 @@ const SignupInfo = () => {
     const data = {
       nickname: nickname,
     };
-    const apiUrl = process.env.REACT_APP_SERVER_URL + "/api/members/checkNickname";
+    const apiUrl = "http://localhost:8080/members/checkNickname";
     axios
       .post(apiUrl, data)
       .then((response) => {
@@ -53,6 +58,44 @@ const SignupInfo = () => {
     if (!isNaN(newValue) && newValue >= 0 && newValue <= 144) {
       setTopScore(newValue);
     }
+  }
+
+  // 평균 타수 함수
+  const handleAverageScoreChange = (e) => {
+    const newValue = e.target.value;
+    if (!isNaN(newValue) && newValue >= 0 && newValue <= 144) {
+      setAverageScore(newValue);
+    }
+  }
+
+  // 이미지 파일 경로를 객체로 관리
+  const iconPaths = {
+    flagred: flagred,
+    flagwhite: flagwhite,
+    flagblack: flagblack,
+    flagall: flagall,
+  };
+
+  const navigate = useNavigate();
+
+  const handleEmailFinish = () => {
+    var referrer = document.referrer;
+
+    console.log("이전 페이지 URL: " + referrer);
+    setIsKakao(isKakao);
+    console.log(isKakao);
+    let level = "";
+    if (averageScore <= 60) {
+      level = "이글 플레이어";
+    } else if (averageScore <= 70) {
+      level = "버디 플레이어";
+    } else if (averageScore <= 80) {
+      level = "파 플레이어";
+    } else if (averageScore <= 90) {
+      level = "보기 플레이어";
+    } else {
+      level = "더블 플레이어";
+    }
     const data = {
       id: email,
       image: image,
@@ -66,7 +109,7 @@ const SignupInfo = () => {
       isKakao: isKakao,
     };
     console.log("isKakao: ", isKakao);
-    const apiUrl = process.env.REACT_APP_SERVER_URL + "/api/members/sign-up";
+    const apiUrl = "http://localhost:8080/members/sign-up";
     axios
       .post(apiUrl, data)
       .then((response) => {
@@ -78,98 +121,107 @@ const SignupInfo = () => {
         console.error("Error: ", error);
       });
 
-    // 평균 타수 함수
-    const handleAverageScoreChange = (e) => {
-      const newValue = e.target.value;
-      if (!isNaN(newValue) && newValue >= 0 && newValue <= 144) {
-        setAverageScore(newValue);
-      }
-    }
-
-    return (
-      <div id="SignupInfo">
-        <div className="user-func-context">
-          자신을 소개하는 정보를 입력하세요
-        </div>
-
-        <input
-          type="email"
-          defaultValue={email}
-          aria-readonly
-          className="user-func-normal-input" />
-        <div className="user-email-auth-block">
-          <input
-            value={nickname}
-            placeholder="닉네임"
-            className="user-email-auth-input"
-            onChange={(e) => setNickname(e.target.value)} />
-          <button className="user-email-auth-button gr"
-            onClick={handleCheckNickname}>
-            검사
-          </button>
-        </div>
-        <input
-          value={introduce}
-          placeholder="자기소개"
-          className="user-func-normal-input"
-          onChange={(e) => setIntroduce(e.target.value)}
-        />
-
-        <div className="user-func-box">
-          <div className="user-func-half">
-            <label className="user-func-label">최고 타수</label>
-            <input
-              type="number"
-              value={topScore}
-              className="user-func-half-input"
-              placeholder="0~144"
-              onChange={handleTopScoreChange} />
-          </div>
-          <div className="user-func-half">
-            <label className="user-func-label">평균 타수</label>
-            <input
-              type="number"
-              value={averageScore}
-              className="user-func-half-input"
-              placeholder="0~144"
-              onChange={handleAverageScoreChange} />
-          </div>
-        </div>
-
-        <div className="user-func-radio">
-          <div className="user-func-label">
-            선호 티 박스
-
-            <div className="tee-tooltip">
-              <AiOutlineInfoCircle className="tee-tooltip-icon" />
-              <span className="tee-tooltip-text">
-                레이디 티(레드 티): 여성이나 어린이를 위한 티 박스로 홀 과의 거리가 가장 짧은 티 박스입니다.<br />
-                레귤러 티(화이트 티): 일반 남성, 여성 상급자, 여성 프로, 청소년 선수 들을 위한 티 박스로 레드 티보다 홀 과의 거리가 멉니다.<br />
-                블랙 티: 몇몇 골프장은 블루 티보다 거리가 먼 블랙 티 박스가 있습니다. 상급자 남성, 프로 선수들이 플레이 합니다.<br />
-                출처: <a className="clickable" href="http://www.golifekorea.com" target="_blank">고라이프코리아</a>
-              </span>
-            </div>
-
-          </div>
-          <div className="user-func-radio-block">
-            <img src={flagred} alt="레드 티 박스"
-              onClick={() => setTeeBox('flagred')}
-              className={`option-tee-img${teeBox === 'flagred' ? '-selected' : ''}`} />
-            <img src={flagwhite} alt="화이트 티 박스"
-              onClick={() => setTeeBox('flagwhite')}
-              className={`option-tee-img${teeBox === 'flagwhite' ? '-selected' : ''}`} />
-            <img src={flagblack} alt="블랙 티 박스"
-              onClick={() => setTeeBox('flagblack')}
-              className={`option-tee-img${teeBox === 'flagblack' ? '-selected' : ''}`} />
-            <img src={flagall} alt="모든 티 박스"
-              onClick={() => setTeeBox('flagall')}
-              className={`option-tee-img${teeBox === 'flagall' ? '-selected' : ''}`} />
-          </div>
-        </div>
-
-      </div>
-    );
+    console.log("data: ", data);
+    // navigate("/");
   };
-}
+
+  return (
+    <div id="SignupInfo">
+      <div className="user-func-context">
+        자신을 소개하는 정보를 입력하세요
+      </div>
+
+      <input
+        type="email"
+        defaultValue={email}
+        aria-readonly
+        className="user-func-normal-input" />
+      <div className="user-email-auth-block">
+        <input
+          value={nickname}
+          placeholder="닉네임"
+          className="user-email-auth-input"
+          onChange={(e) => setNickname(e.target.value)} />
+        <button className="user-email-auth-button gr"
+          onClick={handleCheckNickname}>
+          검사
+        </button>
+      </div>
+      <input
+        value={introduce}
+        placeholder="자기소개"
+        className="user-func-normal-input"
+        onChange={(e) => setIntroduce(e.target.value)}
+      />
+
+      <div className="user-func-box">
+        <div className="user-func-half">
+          <label className="user-func-label">최고 타수</label>
+          <input
+            type="number"
+            value={topScore}
+            className="user-func-half-input"
+            onChange={handleTopScoreChange} />
+        </div>
+        <div className="user-func-half">
+          <label className="user-func-label">평균 타수</label>
+          <input
+            type="number"
+            value={averageScore}
+            className="user-func-half-input"
+            onChange={handleAverageScoreChange} />
+        </div>
+      </div>
+
+      <div className="user-func-radio">
+        <div className="user-func-label">
+          선호 티 박스
+
+          <div className="tee-tooltip">
+            <AiOutlineInfoCircle className="tee-tooltip-icon" />
+            <span className="tee-tooltip-text">
+              레이디 티(레드 티): 여성이나 어린이를 위한 티 박스로 홀 과의 거리가 가장 짧은 티 박스입니다.<br />
+              레귤러 티(화이트 티): 일반 남성, 여성 상급자, 여성 프로, 청소년 선수 들을 위한 티 박스로 레드 티보다 홀 과의 거리가 멉니다.<br />
+              블랙 티: 몇몇 골프장은 블루 티보다 거리가 먼 블랙 티 박스가 있습니다. 상급자 남성, 프로 선수들이 플레이 합니다.<br />
+              출처: <a className="clickable" href="http://www.golifekorea.com" target="_blank">고라이프코리아</a>
+            </span>
+          </div>
+
+        </div>
+        <div className="user-func-radio-block">
+          <img src={flagred} alt="레드 티 박스"
+            onClick={() => setTeeBox('flagred')}
+            className={`option-tee-img${teeBox === 'flagred' ? '-selected' : ''}`} />
+          <img src={flagwhite} alt="화이트 티 박스"
+            onClick={() => setTeeBox('flagwhite')}
+            className={`option-tee-img${teeBox === 'flagwhite' ? '-selected' : ''}`} />
+          <img src={flagblack} alt="블랙 티 박스"
+            onClick={() => setTeeBox('flagblack')}
+            className={`option-tee-img${teeBox === 'flagblack' ? '-selected' : ''}`} />
+          <img src={flagall} alt="모든 티 박스"
+            onClick={() => setTeeBox('flagall')}
+            className={`option-tee-img${teeBox === 'flagall' ? '-selected' : ''}`} />
+        </div>
+      </div>
+
+      <Button
+        onClick={handleEmailFinish}
+        style={{
+          height: "2.5rem",
+          width: "100%",
+
+          color: "black",
+          borderRadius: "30px",
+          background: "#B8F500",
+        }}
+        maxW={"sm"}
+        marginBottom={"2.5rem"}
+      >
+        회원가입 완료하기
+      </Button>
+
+    </div>
+  );
+};
 
 export default SignupInfo;
