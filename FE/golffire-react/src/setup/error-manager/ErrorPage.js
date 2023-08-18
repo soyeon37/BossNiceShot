5fa79644294999f9f5e576d79bdad02b5e448392
김재아 <rkddnjs2816@naver.com>
2023-08-18 13:18:35 +0900

    Fix : 마이페이지에서 png 에러 해결
    Former-commit-id: 7b5a0bf3b3663fa1d8364bf1ce35a112e18c55e9

--- FE/golffire-react/src/setup/error-manager/ErrorPage.js
+++ FE/golffire-react/src/setup/error-manager/ErrorPage.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import ErrorImg from "../../assets/source/mascot/404.png";
 function ErrorPage() {
   return (
-    <div>
-      오류가 발생했습니다.
-      <a href="/">
-        <button>홈 화면으로 돌아가기</button>
-      </a>
+    <div id="error-page">
+      <div id="error-page-top">
+        <img id="error-page-img" src={ErrorImg}></img>
+        <span id="error-page-text">오류가 발생했습니다.</span>
+      </div>
+      <div id="error-page-bottom">
+        <a href="/" >
+          <button id="go-main-button">홈 화면으로 돌아가기</button>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ FE/golffire-react/src/setup/error-manager/ParseProfile.js
@@ -0,0 +1,29 @@
+
+// 프로필 이미지 반환
+function getImage(profile) {
+    if (typeof profile === 'string' && profile.length >= 20) {
+        let profileValues = null;
+        if (profile) profileValues = profile.toString().split(' ');
+        if (profileValues[0] && profileValues[1])
+            return profileValues[0];
+        else
+            return "green_suncap_tiger";
+    }
+    return "green_suncap_tiger";
+}
+
+// 프로필 배경 반환
+function getBackground(profile) {
+    if (typeof profile === 'string' && profile.length >= 20) {
+        let profileValues = null;
+        if (profile) profileValues = profile.toString().split(' ');
+        if (profileValues[0] && profileValues[1])
+            return profileValues[1];
+        else
+            return "white";
+    }
+    return "white";
+}
+
+
+export { getImage, getBackground };
